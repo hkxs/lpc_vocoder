8f8e4c72e495e28596e0a517614a6f0d944f84c7
hkxs <lalvarezf25@gmail.com>
2024-11-06 11:39:25 -0600

    Fix encoder __main__.py

--- src/lpc_vocoder/encode/__main__.py
+++ src/lpc_vocoder/encode/__main__.py
@@ -23,7 +23,7 @@ import argparse
 import logging
 from pathlib import Path
 
-from lpc_vocoder.encode.lpc_encoder import encode_signal
+from lpc_vocoder.encode.lpc_encoder import LpcEncoder
 
 logger = logging.getLogger(__name__)
 
@@ -31,8 +31,11 @@ logger = logging.getLogger(__name__)
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.description = "Encode a .wav signal using LPC"
+    parser.add_argument("audio_file", type=Path, help="Name of the input file")
     parser.add_argument("filename", type=Path, help="Name of the output file")
-    # TODO add the following arguments, order, frame size, overlap percentage
+    parser.add_argument("--order",  default=10, action="store_true", help=f"Order of the LPC filter, default '{10}'")
+    parser.add_argument("--frame_size", action="store_true", help="Frame Size, if not provided it will use a 30ms window based on the sample rate")
+    parser.add_argument("--overlap", default=50, action="store_true", help=f"Overlap as percentage (0-100), default '{50}'")
     parser.add_argument("-d", "--debug", action="store_true", help="Enable debug messages")
     args = parser.parse_args()
 
@@ -45,9 +48,11 @@ def parse_args():
 
 def main():
     args = parse_args()
-    logger.info(f"Encoding file '{args.filename}'")
-    encode_signal(args.filename, window_size=256, overlap=0)
-
+    logger.info(f"Encoding file '{args.audio_file}'")
+    encoder = LpcEncoder(order=args.order)
+    encoder.load_file(args.audio_file, window_size=args.frame_size, overlap=args.overlap)
+    encoder.encode_signal()
+    encoder.save_data(args.filename)
 
 if __name__ == '__main__':
     main()
